6d5870451edfad9a1cf67ff24f7539798e4b5c71
Nicole <nsinche25@ilb.cat>
2026-03-24 12:32:33 +0100

    Primera prueba

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-    ><!DOCTYPE html>
+    <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -6,6 +6,16 @@
         <title>Document</title>
     </head>
     <body>
-        
+    <?php
+    echo "<h1>Tabla de multiplicar</h1>";
+        $numero = 7;
+        // Verificar si la variables es un número entero
+        var_dump(is_int($numero));
+        echo "<h2>Tabla del $numero</h2>";
+            for ($i = 0; $i <= 12; $i++) {
+                $resultado = $numero * $i;
+                echo "$numero x $i = $resultado <br>";
+            }
+        ?>
     </body>
     </html>
