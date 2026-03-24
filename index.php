1feca50bcfb67bb9bcaac6f3f33d58d96161a4b0
Nicole <nsinche25@ilb.cat>
2026-03-24 12:51:59 +0100

    Prueba 2

--- index.php
+++ index.php
@@ -6,16 +6,29 @@
         <title>Document</title>
     </head>
     <body>
-    <?php
-    echo "<h1>Tabla de multiplicar</h1>";
-        $numero = 7;
-        // Verificar si la variables es un número entero
-        var_dump(is_int($numero));
-        echo "<h2>Tabla del $numero</h2>";
-            for ($i = 0; $i <= 12; $i++) {
+    <table>
+        <tr>
+            <th>Multiplicando</th>
+            <th>Multiplicador</th>
+            <th>Resultado</th>
+        </tr>
+        <?php
+        echo "<h1>Tabla de multiplicar</h1>";
+        //Creo la variable
+            $numero = 7;
+        echo "<td>Multiplicando: $numero</td>";
+            for ($i = 1; $i <= 12; $i++) {
                 $resultado = $numero * $i;
-                echo "$numero x $i = $resultado <br>";
-            }
+                }
+        echo "<td>Multiplicador: $i</td>";
+        echo "<td>Resultado: $resultado</td>";
         ?>
+    </table>
     </body>
     </html>
+
+    //Estructura HTML: La taula s'ha de mostrar dins d'una etiqueta <table> de HTML real, amb les seves files (<tr>) i cel·les (<td>)
+    //Bucle for: Utilitza un bucle per generar les files de la taula de l'1 al 10
+    //Estil Visual (Lògica condicional): * Les files parelles han de tenir un color de fons diferent (per exemple, gris clar) de les senars. 
+    //Hauràs d'utilitzar un if amb l'operador mòdul (%) per decidir quina classe CSS aplicar.
+    //Interactivitat bàsica: Si el número definit a la variable és superior a 12 o inferior a 1, la pàgina ha de mostrar un missatge d'error en lloc de la taula.
